e38dcc7c522948db3d1b9666337c4762904b0172
Abdullah Al Rafi <rafi.workfile@gmail.com>
2024-09-25 11:51:28 +0600

    almost done

--- script.js
+++ script.js
@@ -2,11 +2,27 @@ donate("dBtn1", "1");
 donate("dBtn2", "2");
 donate("dBtn3", "3");
 
-document.getElementById('donatePage').addEventListener('click',function () {
-    document.getElementById('donatePage').classList.add('btn-accent','bg-lime-400');
-    document.getElementById('historyPage').classList.remove('btn-accent','bg-lime-400');
+document.getElementById("donatePage").addEventListener("click", function () {
+  document
+    .getElementById("donatePage")
+    .classList.add("btn-accent", "bg-lime-400");
+  document
+    .getElementById("historyPage")
+    .classList.remove("btn-accent", "bg-lime-400");
+  document.getElementById("donation-section").classList.remove("hidden");
+  document.getElementById("history-section").classList.add("hidden");
 });
-document.getElementById('historyPage').addEventListener('click',function () {
-    document.getElementById('donatePage').classList.remove('btn-accent','bg-lime-400');
-    document.getElementById('historyPage').classList.add('btn-accent','bg-lime-400');
+document.getElementById("historyPage").addEventListener("click", function () {
+  document
+    .getElementById("donatePage")
+    .classList.remove("btn-accent", "bg-lime-400");
+  document
+    .getElementById("historyPage")
+    .classList.add("btn-accent", "bg-lime-400");
+  document.getElementById("donation-section").classList.add("hidden");
+  document.getElementById("history-section").classList.remove("hidden");
+});
+
+document.getElementById("blogs").addEventListener("click", function () {
+  window.location.href = "blog.html";
 });
--- utilities.js
+++ utilities.js
@@ -7,7 +7,7 @@ function donate(btnId, cashId) {
       document.getElementById(cashId).value = "";
       return;
     }
-    if(parseFloat(document.getElementById("balance").innerText) < donation){
+    if (parseFloat(document.getElementById("balance").innerText) < donation) {
       alert("Insufficient balance!!");
       document.getElementById(cashId).value = "";
       return;
@@ -17,5 +17,30 @@ function donate(btnId, cashId) {
     document.getElementById("fund" + cashId).innerText = fund + donation;
     document.getElementById(cashId).value = "";
     my_modal_5.showModal();
+
+    const donateDiv = document.createElement("div");
+    donateDiv.classList.add(
+      "rounded-lg",
+      "border",
+      "border-gray-400",
+      "p-4",
+      "my-3"
+    );
+
+    const h1 = document.createElement("h1");
+    h1.classList.add("font-bold", "text-md");
+    if (cashId == 1)
+      h1.innerText = `${donation} Taka is Donated for Flood Relief in Feni, Bangladesh`;
+    if (cashId == 2)
+      h1.innerText = `${donation} Taka is Donated for famine-2024 in Feni, Bangladesh`;
+    if (cashId == 3)
+      h1.innerText = `${donation} Taka is Donated for Quota movement in Feni, Bangladesh`;
+    const date = new Date();
+    const p = `Date: ${date}`;
+
+    document.getElementById("pre").classList.add("hidden");
+    donateDiv.append(h1);
+    donateDiv.append(p);
+    document.getElementById("history-container").appendChild(donateDiv);
   });
 }
